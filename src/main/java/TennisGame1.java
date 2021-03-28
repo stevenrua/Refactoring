17ce1757c490ad4f025e351be351fedbbfeb5d7c
steven <steven-rua@hotmail.com>
2021-03-27 21:53:26 -0500

    Se refactoriza la clase TennisGame1

--- src/main/java/TennisGame1.java
+++ src/main/java/TennisGame1.java
@@ -1,76 +1,96 @@
 
 public class TennisGame1 implements TennisGame {
     
-    private int m_score1 = 0;
-    private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
+    private int playerOnePoints = 0;
+    private int playerTwoPoints = 0;
 
     public TennisGame1(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
+        if (playerName.equals("player1"))
+            playerOnePoints += 1;
         else
-            m_score2 += 1;
+            playerTwoPoints += 1;
     }
 
     public String getScore() {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
+
+        if (compareScoreEquals())
+            return scoreEquals(score);
+
+        if (compareScoreGreaterThan4())
+           return scoreGreaterThan4(score);
+
+        return otherScore(score);
+    }
+
+    public String scoreEquals(String score){
+        switch (playerTwoPoints)
         {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
-        }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+            case 0:
+                score = "Love-All";
+                break;
+            case 1:
+                score = "Fifteen-All";
+                break;
+            case 2:
+                score = "Thirty-All";
+                break;
+            default:
+                score = "Deuce";
+                break;
+
         }
         return score;
     }
+
+    public String scoreGreaterThan4(String score){
+        int advantageplayer = playerOnePoints - playerTwoPoints;
+        if (advantageplayer == 1) return "Advantage player1";
+        if (advantageplayer == -1) return "Advantage player2";
+        if (advantageplayer >= 2) return  "Win for player1";
+        return "Win for player2";
+    }
+
+    public String otherScore(String score){
+        int currentPlayerScore =0;
+        StringBuilder scoreBuilder = new StringBuilder(score);
+        for (int i = 1; i<3; i++)
+        {
+            if (i==1) currentPlayerScore = playerOnePoints;
+            else {
+                scoreBuilder.append("-");
+                currentPlayerScore = playerTwoPoints;
+            }
+            switch(currentPlayerScore)
+            {
+                case 0:
+                    scoreBuilder.append("Love");
+                    break;
+                case 1:
+                    scoreBuilder.append("Fifteen");
+                    break;
+                case 2:
+                    scoreBuilder.append("Thirty");
+                    break;
+                case 3:
+                    scoreBuilder.append("Forty");
+                    break;
+            }
+        }
+        score = scoreBuilder.toString();
+        return score;
+    }
+
+    public boolean compareScoreEquals(){
+        return (playerOnePoints == playerTwoPoints);
+    }
+
+    public boolean compareScoreGreaterThan4(){
+        return (playerOnePoints >=4 || playerTwoPoints >=4);
+    }
+
+
 }
